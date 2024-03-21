4cdc62700c319658e37bc07e76ceccd975442c59
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2024-03-21 08:23:31 +0100

    Uploaded pre-lecture files for lecture 20

---
+++ Forelesninger/Forelesning 20 - API og Vue/Filer før forelesning/hello-world.vue
@@ -0,0 +1,12 @@
+<template id="template">
+  <h1 class="header">Hello, Vue!</h1>
+</template>
+<script>
+  app.component("hello-world", {template: "#template"})
+</script>
+<style>
+  .header {
+    color: cornflowerblue;
+    font-size: 34px;
+  }
+</style>
